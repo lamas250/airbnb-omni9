197290814e5a6dc311c2d351bd9952a6277ae94d
Igor <igorlamas1994@gmail.com>
2021-03-03 19:11:10 -0300

    new spot, dashboard page

--- packages/web/src/pages/Dashboard/index.js
+++ packages/web/src/pages/Dashboard/index.js
@@ -1,21 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import './styles.css';
 
 
 const Dashboard = () => {
+  const [spots, setSpots] = useState([]);
 
   useEffect(() => {
     async function loadSpots(){
       const user_id = localStorage.getItem('user');
-      const response = await api.get('/dashboard');
+      const response = await api.get('/dashboard', {
+        headers: {user_id}
+      });
+      response.data.map(spot => {
+        return spot['thumbnail_url'] = spot['thumbnail_url'].replace(' ','%20');
+      })
+      setSpots(response.data);
+      console.log(response.data);
     }
-
+    loadSpots();
   },[])
 
   return (
-    <div>
-      Oi bb
-    </div>
+    <>
+      <ul className="spotList">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$ ${spot.price}` : 'Free'}</span>
+          </li>
+        ))}
+      </ul>
+      <Link to="/new">
+        <button className="btn">Cadastrar novo spot</button>
+      </Link>
+    </>
   )
 }
 
--- packages/web/src/pages/NewSpot/index.js
+++ packages/web/src/pages/NewSpot/index.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const New = () => {
+  const [form, setForm] = useState({company: '',})
+
+  function handleSubmit(){
+
+  }
+
+  function handleForm(e){
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>Empresa*</label>
+      <input 
+        name={"company"}
+        placeholder="Sua empresa aqui"
+        value={form.company}
+        onChange={handleForm}
+      />
+      <label>Tecnologias* <span>Separado por virgulas</span></label>
+      <input 
+        name={"company"}
+        placeholder="Sua empresa aqui"
+        value={form.company}
+        onChange={handleForm}
+      />
+    </form>
   )
 }
 
