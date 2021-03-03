30bfc199eeb5d288538026e3bf15681f17c30118
Igor <igorlamas1994@gmail.com>
2021-03-02 22:39:15 -0300

    server -> session e spot create

--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const SessionController = require('./app/controllers/SessionController');
+const SpotController = require('./app/controllers/SpotController');
 
 const routes = express.Router();
 
-routes.post('/users', (req,res) => {
-  return res.json(req.body);
-})
+routes.post('/session', SessionController.store);
+
+routes.post('/spots', SpotController.store);
 
 module.exports = routes;
